16525439d5d3703df060b9a19c60957a786035d2
Kategamash <MVGolovanovList@gmail.com>
2024-05-02 21:57:45 +0300

    Init + Level Generation

---
+++ PP/Assets/Scrips/RoomSpawner.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+
+    public Direction direction;
+
+    public enum Direction
+    {
+        Top,
+        Bottom,
+        Left,
+        Right,
+        None
+    }
+
+    private RoomVariants variants;
+    private int rand;
+    private bool spawned = false;
+    private float waitTime = 3f;
+
+    private void Start()
+    {
+        variants = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomVariants>();
+        Destroy(gameObject, waitTime);
+        Invoke("Spawn", 0.2f);
+    }
+
+    public void Spawn()
+    {
+        if (!spawned)
+        {
+            if (direction == Direction.Top)
+            {
+                rand = Random.Range(0, variants.topRooms.Length);
+                Instantiate(variants.topRooms[rand], transform.position, variants.topRooms[rand].transform.rotation);
+            }
+            else if (direction == Direction.Bottom)
+            {
+                rand = Random.Range(0, variants.bottomRooms.Length);
+                Instantiate(variants.bottomRooms[rand], transform.position, variants.bottomRooms[rand].transform.rotation);
+            }
+            else if (direction == Direction.Left)
+            {
+                rand = Random.Range(0, variants.leftRooms.Length);
+                Instantiate(variants.leftRooms[rand], transform.position, variants.leftRooms[rand].transform.rotation);
+            }
+            else if (direction == Direction.Right)
+            {
+                rand = Random.Range(0, variants.rightRooms.Length);
+                Instantiate(variants.rightRooms[rand], transform.position, variants.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("RoomPoint") && other.GetComponent<RoomSpawner>().spawned)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ PP/Assets/Scrips/RoomVariants.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomVariants : MonoBehaviour
+{
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] bottomRooms;
+
+
+}
