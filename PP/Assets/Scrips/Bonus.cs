fb08576b7800cfc8726496262d71406fe881c38e
Nikita <nickitasolonenko@yandex.ru>
2024-05-25 19:00:36 +0300

    добавил лут монет, счетчик

--- PP/Assets/Scrips/Bonus.cs
+++ PP/Assets/Scrips/Bonus.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bonus : MonoBehaviour
 {
     public string bonusName;
+
+    public Text coinCount;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,6 +18,7 @@ public class Bonus : MonoBehaviour
                 case "coin":
                     int coins = PlayerPrefs.GetInt("coins");
                     PlayerPrefs.SetInt("coins", coins + 1);
+                    coinCount.text = (coins + 1).ToString();
                     Destroy(gameObject);
                     break;
             }
